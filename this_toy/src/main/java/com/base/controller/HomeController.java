53e347a8c8c484f5f857f5a44611ed028827635e
Jin\User <hate7354@naver.com>
2021-07-20 18:52:48 +0900

    테스트 1

--- this_toy/src/main/java/com/base/controller/HomeController.java
+++ this_toy/src/main/java/com/base/controller/HomeController.java
@@ -26,6 +26,8 @@ public class HomeController {
 	public String home(Locale locale, Model model) {
 		logger.info("Welcome home! The client locale is {}.", locale);
 		
+		System.out.println("¾È³ç");
+		
 		Date date = new Date();
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
 		
