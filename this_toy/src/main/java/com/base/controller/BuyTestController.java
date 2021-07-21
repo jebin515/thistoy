3600ab06e15b1e2dd38d2d477582c01a348f38d1
jebin515 <jebin515@naver.com>
2021-07-22 01:08:13 +0900

    tt

--- this_toy/src/main/java/com/base/controller/BuyTestController.java
+++ this_toy/src/main/java/com/base/controller/BuyTestController.java
@@ -1,17 +1,19 @@
 package com.base.controller;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-@Controller
-	@RequestMapping("/buy/*")
-	public class BuyTestController {
-		
-		@RequestMapping("/buy")
-		public void buy() {
-			
-		}
-		@RequestMapping("/buysuccess")
-		public void buysuccess() {
-			
-		}
-}
 
+@Controller
+@RequestMapping("/buy/*")
+public class BuyTestController {
+
+	@RequestMapping("/buy")
+	public void buy() {
+
+	}
+
+	@RequestMapping("/buysuccess")
+	public void buysuccess() {
+
+	}
+}
