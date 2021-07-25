347f54defa9b4d73283d3354cc6a04207edffb9f
kilin180 <80955556+kilin180@users.noreply.github.com>
2021-07-26 04:12:57 +0900

    소셜로그인

--- this_toy/src/main/java/com/test/tst/NaverLoginController.java
+++ this_toy/src/main/java/com/test/tst/NaverLoginController.java
@@ -21,79 +21,80 @@ import com.github.scribejava.core.model.OAuth2AccessToken;
 @Controller
 @RequestMapping("/tst/*")
 public class NaverLoginController {
-/* NaverLoginBO */
-private NaverLoginBO naverLoginBO;
-private String apiResult = null;
-@Autowired
-private void setNaverLoginBO(NaverLoginBO naverLoginBO) {
-this.naverLoginBO = naverLoginBO;
-}
+	/* NaverLoginBO */
+	private NaverLoginBO naverLoginBO;
+	private String apiResult = null;
 
-@GetMapping("/login")
-public String loginGET() {
-	System.out.println("네이버로그인 페이지");
-	return "/tst/login";
-}
-@GetMapping("/index")
-public String indexGET() {
-	System.out.println("네이버기본 페이지");
-	return "/tst/index";
-}
-@GetMapping("/callback")
-public String callbackGET() {
-	System.out.println("콜백 페이지");
-	return "/tst/callback";
-}
+	@Autowired
+	private void setNaverLoginBO(NaverLoginBO naverLoginBO) {
+		this.naverLoginBO = naverLoginBO;
+	}
+
+	@GetMapping("/login")
+	public void loginGET() {
+		System.out.println("네이버로그인 페이지");
+	}
+
+	@GetMapping("/index")
+	public void indexGET() {
+		System.out.println("네이버기본 페이지");
+	}
+
+	@GetMapping("/callback")
+	public void callbackGET() {
+		System.out.println("콜백 페이지");
+	}
 
 //로그인 첫 화면 요청 메소드
 //@RequestMapping(value = "/login", method = { RequestMethod.GET, RequestMethod.POST })
-@PostMapping("/login")
-public String login(Model model, HttpSession session) {
-/* 네이버아이디로 인증 URL을 생성하기 위하여 naverLoginBO클래스의 getAuthorizationUrl메소드 호출 */
-String naverAuthUrl = naverLoginBO.getAuthorizationUrl(session);
+	@PostMapping("/login")
+	public String login(Model model, HttpSession session) {
+		/* 네이버아이디로 인증 URL을 생성하기 위하여 naverLoginBO클래스의 getAuthorizationUrl메소드 호출 */
+		String naverAuthUrl = naverLoginBO.getAuthorizationUrl(session);
 //https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=sE***************&
 //redirect_uri=http%3A%2F%2F211.63.89.90%3A8090%2Flogin_project%2Fcallback&state=e68c269c-5ba9-4c31-85da-54c16c658125
-System.out.println("네이버:" + naverAuthUrl);
+		System.out.println("네이버:" + naverAuthUrl);
 //네이버
-model.addAttribute("url", naverAuthUrl);
-return "login";
-}
+		model.addAttribute("url", naverAuthUrl);
+		return "login";
+	}
+
 //네이버 로그인 성공시 callback호출 메소드
 //@RequestMapping(value = "/callback", method = { RequestMethod.GET, RequestMethod.POST })
-@PostMapping("/callback")
-public String callback(Model model, @RequestParam String code, @RequestParam String state,
-		HttpSession session) throws IOException, ParseException {
-System.out.println("여기는 callback");
-OAuth2AccessToken oauthToken;
-oauthToken = naverLoginBO.getAccessToken(session, code, state);
+	@RequestMapping("/callback")
+	public String callback(Model model, @RequestParam String code, @RequestParam String state, HttpSession session)
+			throws IOException, ParseException {
+		System.out.println("여기는 callback");
+		OAuth2AccessToken oauthToken;
+		oauthToken = naverLoginBO.getAccessToken(session, code, state);
 //1. 로그인 사용자 정보를 읽어온다.
-apiResult = naverLoginBO.getUserProfile(oauthToken); //String형식의 json데이터
-/** apiResult json 구조
-{"resultcode":"00",
-"message":"success",
-"response":{"id":"33666449","nickname":"shinn****","age":"20-29","gender":"M","email":"sh@naver.com","name":"\uc2e0\ubc94\ud638"}}
-**/
+		apiResult = naverLoginBO.getUserProfile(oauthToken); // String형식의 json데이터
+		/**
+		 * apiResult json 구조 {"resultcode":"00", "message":"success",
+		 * "response":{"id":"33666449","nickname":"shinn****","age":"20-29","gender":"M","email":"sh@naver.com","name":"\uc2e0\ubc94\ud638"}}
+		 **/
 //2. String형식인 apiResult를 json형태로 바꿈
-JSONParser parser = new JSONParser();
-Object obj = parser.parse(apiResult);
-JSONObject jsonObj = (JSONObject) obj;
+		JSONParser parser = new JSONParser();
+		Object obj = parser.parse(apiResult);
+		JSONObject jsonObj = (JSONObject) obj;
 //3. 데이터 파싱
 //Top레벨 단계 _response 파싱
-JSONObject response_obj = (JSONObject)jsonObj.get("response");
+		JSONObject response_obj = (JSONObject) jsonObj.get("response");
 //response의 nickname값 파싱
-String nickname = (String)response_obj.get("nickname");
-System.out.println(nickname);
+		String nickname = (String) response_obj.get("nickname");
+		System.out.println(nickname);
 //4.파싱 닉네임 세션으로 저장
-session.setAttribute("sessionId",nickname); //세션 생성
-model.addAttribute("result", apiResult);
-return "login";
-}
+		session.setAttribute("sessionId", nickname); // 세션 생성
+		model.addAttribute("result", apiResult);
+		return "login";
+	}
+
 //로그아웃
 //@RequestMapping(value = "/logout", method = { RequestMethod.GET, RequestMethod.POST })
-@PostMapping("/logout")
-public String logout(HttpSession session)throws IOException {
-System.out.println("여기는 logout");
-session.invalidate();
-return "redirect:index.jsp";
-}
+	@PostMapping("/logout")
+	public String logout(HttpSession session) throws IOException {
+		System.out.println("여기는 logout");
+		session.invalidate();
+		return "redirect:index.jsp";
+	}
 }
