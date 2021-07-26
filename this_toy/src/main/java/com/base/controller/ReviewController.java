3dc0883907f4831f8472bc3be146ef596d070f4b
jebin515 <jebin515@naver.com>
2021-07-26 13:53:41 +0900

    review

---
+++ this_toy/src/main/java/com/base/controller/ReviewController.java
@@ -0,0 +1,5 @@
+package com.base.controller;
+
+public class ReviewController {
+
+}
---
+++ this_toy/src/main/java/com/base/mapper/ReviewMapper.java
@@ -0,0 +1,8 @@
+package com.base.mapper;
+
+import com.base.entity.ReviewVO;
+
+public interface ReviewMapper {
+	
+	  int insert(ReviewVO vo);
+}
