6e36000feeb2745a36b821e27e0820b4c35debb0
KimSeHee <KimSeHee@DESKTOP-QTP1MKD>
2021-07-26 16:29:24 +0900

    second edit noticeMapper.xml,noticeMapper.java,noticeVO

--- this_toy/src/main/java/com/base/mapper/NoticeMapper.java
+++ this_toy/src/main/java/com/base/mapper/NoticeMapper.java
@@ -8,9 +8,9 @@ import com.base.entity.NoticeVO;
 
 public interface NoticeMapper {
 
-		//@Select("select * from notice where notice_num > 0")
 		List<NoticeVO> getList();
 		
-		void insert(NoticeVO vo);
+		void insert(NoticeVO notice);
 		
+		void insertSelectKey(NoticeVO notice);
 }
