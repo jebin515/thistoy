2acd7099aeae552271f04050851de4f853bdd2a3
KimSeHee <KimSeHee@DESKTOP-QTP1MKD>
2021-07-27 23:28:30 +0900

    edit notice

--- this_toy/src/main/java/com/base/service/notice/NoticeServiceImpl.java
+++ this_toy/src/main/java/com/base/service/notice/NoticeServiceImpl.java
@@ -1,7 +1,10 @@
 package com.base.service.notice;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
+import com.base.entity.NoticeVO;
 import com.base.mapper.NoticeMapper;
 
 import lombok.RequiredArgsConstructor;
@@ -15,4 +18,35 @@ import lombok.extern.log4j.Log4j;
 public class NoticeServiceImpl implements NoticeService{
 
 	private final NoticeMapper mapper;
+
+	@Override
+	public Long register(NoticeVO notice) {
+		
+		mapper.insertSelectKey(notice);
+		
+		return notice.getNoticeNum();
+	}
+
+	@Override
+	public NoticeVO get(Long noticeNum) {
+		
+		return mapper.read(noticeNum);
+	}
+
+	@Override
+	public int modify(NoticeVO notice) {
+		return mapper.update(notice);
+	}
+
+	@Override
+	public int remove(Long noticeNum) {
+	
+		return mapper.delete(noticeNum);
+	}
+
+	@Override
+	public List<NoticeVO> getList() {
+
+		return mapper.getList();
+	}
 }
