d9326224bc743c0ffc561674a7997c082ac5d9e5
jiyoojin <a82362336@gmail.com>
2021-07-28 14:42:54 +0900

    admin수정중

--- this_toy/src/main/java/com/base/mapper/AdminMapper.java
+++ this_toy/src/main/java/com/base/mapper/AdminMapper.java
@@ -1,7 +1,12 @@
-/*
- * package com.base.mapper;
- * 
- * public interface AdminMapper {
- * 
- * }
- */
+package com.base.mapper;
+
+import java.util.ArrayList;
+
+import com.base.entity.UserVO;
+
+public interface AdminMapper {
+	 
+	 ArrayList<UserVO> admin();
+  
+ }
+ 
--- this_toy/src/main/java/com/base/service/admin/AdminServiceImpl.java
+++ this_toy/src/main/java/com/base/service/admin/AdminServiceImpl.java
@@ -1,18 +1,25 @@
-/*
- * package com.base.service.admin;
- * 
- * import org.springframework.stereotype.Service;
- * 
- * import com.base.mapper.AdminMapper;
- * 
- * import lombok.AllArgsConstructor;
- * 
- * @Service
- * 
- * @AllArgsConstructor public class AdminServiceImpl implements AdminService{
- * 
- * private AdminMapper mapper;
- * 
- * }
- */
+package com.base.service.admin;
 
+import java.util.ArrayList;
+
+import org.springframework.stereotype.Service;
+
+import com.base.entity.UserVO;
+import com.base.mapper.AdminMapper;
+
+import lombok.AllArgsConstructor;
+
+@Service
+
+@AllArgsConstructor
+public class AdminServiceImpl implements AdminService {
+
+	private AdminMapper mapper;
+	
+	@Override
+	public ArrayList<UserVO> admin() {
+		// TODO Auto-generated method stub
+		return mapper.admin();
+	}
+	
+}
