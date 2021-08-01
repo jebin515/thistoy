3235b84cd4cf2b9a1c368050c4ec39b8ad6eff20
konkeysta <konkeysta@naver.com>
2021-08-01 14:12:44 +0900

    test

--- this_toy/src/main/java/com/base/controller/MypageController.java
+++ this_toy/src/main/java/com/base/controller/MypageController.java
@@ -74,6 +74,18 @@ public class MypageController {
 		
 	}
 	
+	/*
+	 * @GetMapping("mypage_detailorderbox") public void getDOrderbox
+	 * (HttpServletRequest request, Model model) { HttpSession session =
+	 * request.getSession(); String userId = (String)
+	 * session.getAttribute("userId"); }
+	 * 
+	 * @GetMapping("mypage_review") public void getOrderbox (HttpServletRequest
+	 * request, Model model) { HttpSession session = request.getSession(); String
+	 * userId = (String) session.getAttribute("userId");
+	 * 
+	 * }
+	 */
 	
 	
 	
